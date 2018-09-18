34d7bc97e781a74791a87c61e9346c40ac53fe79
Willians Martins <professorwillians@gmail.com>
2018-09-17 23:47:30 -0300

    inicio

---
+++ meu-primeiro/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector:'meu-primeiro-component',
+	template: `
+		<p>meu primeiro componente com angular2</p>
+	`
+})
+export class MeuPrimeiroComponent{
+	
+}
