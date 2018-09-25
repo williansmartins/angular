8328a497ebd073399220f53c9e90f96c51fc78f4
Willians Martins <willians.morais-e@riachuelo.com.br>
2018-09-25 17:52:21 -0300

    criando uma service nao assincrona

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,17 +14,22 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
+  constructor(private heroService: HeroService) { }
 
-  constructor() { }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
